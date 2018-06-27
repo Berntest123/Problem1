17b7f93e77b994c8753b9361b67188b49a36ec9f
berntest123 <bbaseplate@yahoo.co.uk>
2018-06-27 10:51:18 +0300

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+import { Post } from './posts/post.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  storedPosts: Post[] = [];  // We got an array of posts in there
+
+
+  onPostAdded(post) {
+    this.storedPosts.push(post);
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,53 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { MatInputModule,
+         MatCardModule,
+         MatButtonModule,
+         MatToolbarModule,
+         MatExpansionModule,
+         MatProgressSpinnerModule,
+         MatPaginatorModule
+} from '@angular/material';
+
+import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { AuthInterceptor } from './auth/auth-interceptor';
+// import { PostService } from './posts/posts.service';
+
+@NgModule({         // We declare the classes
+  declarations: [
+    AppComponent,
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent,
+    LoginComponent,
+    SignupComponent
+  ],
+  imports: [            // We import the components
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    HttpClientModule
+  ],
+  // providers: [PostService],                   // providers are used for services
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],  // Dont overwrite interceptors, add an additional one
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
